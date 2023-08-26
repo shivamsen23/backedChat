4c79dd0cf54505fd950ed8dd0655772278eecafe
shivamsen23 <97625468+shivamsen23@users.noreply.github.com>
2023-08-26 21:05:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-res.setHeader("Access-Control-Allow-Credentials","true");
 app.use('/users', userRoutes)
  require('./connection')
 
